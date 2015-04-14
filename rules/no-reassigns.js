b2497ce23134f22726f0d563ff80ae286f6615b3
idmitriev <idmitriev@okko.tv>
2015-04-14 11:48:41 +0300

    module.export reassigns

--- rules/no-reassigns.js
+++ rules/no-reassigns.js
@@ -1,7 +1,9 @@
 module.exports = function(context) {
 	return {
 		'AssignmentExpression': function(node) {
-			context.report(node, 'Unexpected variable reassignment');
+			return node.left != null && node.left.type === 'MemberExpression' && node.left.property != null && node.left.property.name === 'exports' ?
+				null :
+				context.report(node, 'Unexpected variable reassignment');
 		}
 	};
 }
